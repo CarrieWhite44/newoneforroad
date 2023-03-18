fa3536faee373d023c8b55090342d53b2eea34ce
Carry White <127890155+CarrieWhite44@users.noreply.github.com>
2023-03-18 11:37:24 +0000

    added jobs

--- app.py
+++ app.py
@@ -1,11 +1,37 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
-
+JOBS = [
+  {
+    'id': 1,
+    'Title': 'Software Engineer',
+    'Location': 'Sydney',
+    'Salary': '$6000'
+  },
+  
+  { 
+    'id': 2,
+    'Title': 'Middle-Python',
+    'Location': 'Monreal',
+  },
+  
+  {
+    'id': 3,
+    'Title': 'Senior-Python',
+    'Location': 'Sydney',
+    'Salary': '$7000'
+  }
+]
 
 @app.route("/")
 def hello_world():
-  return "Hello, world!"
+  return render_template('home.html',
+                         jobs=JOBS,
+                         company_name='Carrie')
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS) 
 
 
 if __name__ == '__main__':
